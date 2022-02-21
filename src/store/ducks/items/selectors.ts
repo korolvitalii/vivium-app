f8170a73d077683940036f9e2b4e1b326a929378
korolvitalii <korolvitalik@gmail.com>
2022-02-21 20:56:52 +0100

    Add and create selectors

--- src/store/ducks/items/selectors.ts
+++ src/store/ducks/items/selectors.ts
@@ -2,3 +2,6 @@ import { AppStateType } from '../../rootReducer';
 import { ItemsState } from './contracts/state';
 
 export const selectItems = (state: AppStateType): ItemsState['data'] => state.items.data;
+
+export const selectLoadingStatus = (state: AppStateType): ItemsState['status'] =>
+  state.items.status;
--- src/store/ducks/user/selectors.ts
+++ src/store/ducks/user/selectors.ts
@@ -1,4 +1,5 @@
 import { AppStateType } from '../../rootReducer';
+import { LoadingStatus } from '../../types';
 import { UserState } from './contracts/state';
 
 export const selectUserLoadingStatus = (state: AppStateType): UserState['status'] =>
@@ -7,3 +8,6 @@ export const selectUserLoadingStatus = (state: AppStateType): UserState['status'
 export const selectUserData = (state: AppStateType): UserState['data'] => state.user.data;
 
 export const selectIsAuth = (state: AppStateType): boolean => !!state.user.data;
+
+export const selectErrors = (state: AppStateType): boolean =>
+  state.user.status === LoadingStatus.ERROR;
