b1e4b614cd9bd41c2e85026a32b9e92deb238299
korolvitalii <korolvitalik@gmail.com>
2022-02-21 20:59:11 +0100

    Change and add random time in data from api.

--- src/store/ducks/items/reducer.ts
+++ src/store/ducks/items/reducer.ts
@@ -1,4 +1,5 @@
-import { LoadingStatus } from '../../types';
+import moment from 'moment';
+import { IBook, LoadingStatus } from '../../types';
 import { ItemsActions, ItemsActionsType } from './actionTypes';
 import { ItemsState } from './contracts/state';
 
@@ -12,7 +13,13 @@ type InitialStateType = typeof initialState;
 export const itemsReducer = (state = initialState, action: ItemsActions): InitialStateType => {
   switch (action.type) {
     case ItemsActionsType.SET_ITEMS: {
-      return { ...state, data: action.payload };
+      const itemsWithRandomTimestamp = action.payload?.reduce((acc: IBook[], item: IBook) => {
+        const randomDate: string = moment(
+          new Date(+new Date() - Math.floor(Math.random() * 10000000000)),
+        ).format('MM/DD/YYYY');
+        return [...acc, { ...item, timestamp: randomDate }];
+      }, []);
+      return { ...state, data: itemsWithRandomTimestamp };
     }
     case ItemsActionsType.SET_LOADING_STATE: {
       return { ...state, status: action.payload };
